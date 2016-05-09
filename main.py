ff72ad6839dda8fb9458a470deb6eeca66de68e8
marcin-iwanski <marcin@acepta.com>
2016-05-09 13:12:16 -0300

    sugerencias de cambios

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 #!/opt/python3/bin/python3
+
 from notificaciones import Notificaciones
-from query import Query
+import query
 from logs import Logs
 
-class Inicio:
+def main():
+	#Intanciar class logs
+	logs = Logs()
+	#Intanciar class notificaciones
+	notificaciones = Notificaciones()
+	#Intanciar class query
+	#Inicio del programa
+	logs.escribe_log('INICIO programa')
+	#Obteniendo documentos a dec3
+	logs.escribe_log('Obteniendo documentos dec3')
+	query.get_doctos_dec()
+	logs.escribe_log('FIN programa')
 
-	def init_prog(self):
-		#Intanciar class logs
-		logs = Logs()
-		#Intanciar class notificaciones
-		notificaciones = Notificaciones()
-		#Intanciar class query
-		sql_q = Query()
-		#Inicio del programa
-		logs.escribe_log('INICIO programa')
-		#Obteniendo documentos a dec3
-		logs.escribe_log('Obteniendo documentos dec3')
-		sql_q.get_doctos_dec()
-		logs.escribe_log('FIN programa')
 
-#Inicio del programa
-init_progama = Inicio()
-init_progama.init_prog()
+main()
